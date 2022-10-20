e56310249aa7da44a29e24289b9d1570ad8c0d22
Carl Joakim Damsleth <kim@damsleth.com>
2022-10-20 15:30:25 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,27 @@
-'use strict';
+'use strict'
 
-const gulp  = require('gulp');
-const build = require('@microsoft/sp-build-web');
+const build = require('@microsoft/sp-build-web')
+const gulp = require('gulp')
+const insertSourceMapsTasks = require('./insertSourceMapsTasks')
+gulp.task('insertSourceMaps', insertSourceMapsTasks)
+build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`)
 
-const webpack              = require('webpack');
-const gulpSequence         = require('gulp-sequence');
-const insertSourceMapsTaks = require('./insertSourceMapsTasks');
-
-build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
-
-/**
- * Force the creation of source map files even for production
- * @see https://blog.mastykarz.nl/debug-production-version-sharepoint-framework-solution/#generatesourcemapforreleasebuild ~
- */
-build.configureWebpack.mergeConfig({
-    additionalConfiguration: (generatedConfiguration) => {
-        generatedConfiguration.devtool = 'source-map';
-        // force
-        for (var i = 0; i < generatedConfiguration.plugins.length; i++) {
-            const plugin = generatedConfiguration.plugins[i];
-            if (plugin instanceof webpack.optimize.UglifyJsPlugin) {
-                plugin.options.sourceMap = true;
-                break;
-            }
-        }
-
-        return generatedConfiguration;
+var getTasks = build.rig.getTasks
+build.rig.getTasks = function () {
+  var result = getTasks.call(build.rig)
+  result.set('serve', result.get('serve-deprecated'))
+  result.additionalConfiguration = (generatedConfiguration) => {
+    generatedConfiguration.devtool = 'source-map'
+    // force sourcemap = true
+    for (var i = 0; i < generatedConfiguration.plugins.length; i++) {
+      const plugin = generatedConfiguration.plugins[i]
+      if (plugin instanceof webpack.optimize.UglifyJsPlugin) {
+        plugin.options.sourceMap = true
+        break
+      }
     }
-});
-
-build.initialize(gulp);
-
-// Creates a gulp task to insert source maps into the final sppkg
-gulp.task('insertSourceMaps', insertSourceMapsTaks);
-
-// Combines all tasks into a single command
-gulp.task('dist', gulpSequence('clean', 'bundle', 'package-solution', 'insertSourceMaps'));
+    return generatedConfiguration
+  }
+  return result
+}
+build.initialize(require('gulp'))
